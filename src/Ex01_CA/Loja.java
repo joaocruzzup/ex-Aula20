654d4ca8269d0f9f731d96a9c53069f609298a43
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-06-16 12:39:33 -0300

    :boom: fix: Melhorado impressao do Menu

--- src/Ex01_CA/Loja.java
+++ src/Ex01_CA/Loja.java
@@ -15,38 +15,54 @@ public class Loja {
         listaProdutos.add(new CDs("4", "Anavitoria - O tempo é agora", 28.5F, 20));
         listaProdutos.add(new CDs("5", "Rubel - Pearl", 30.5F, 30));
 
-        System.out.println("-------------------------------------------");
-        System.out.println("Boas vindas ao seu gerenciador de estoques!");
-        System.out.println("-------------------------------------------");
-
-        System.out.println("Escolha a opção que você deseja!");
-        System.out.println("1. Ver estoque \n2. Buscar produto \n3. Duplicar Produtos");
-        System.out.println("Digite a opção: ");
         Scanner sc = new Scanner(System.in);
-        int opcao = sc.nextInt();
+        int opcao = 0;
 
-        switch (opcao){
-            case 1:
-                System.out.println("--------------------------- Menu Estoque ---------------------------");
-                imprimirProdutos(listaProdutos);
-                break;
-            case 2:
-                System.out.println("---------- Menu Buscar por Código de Barras ----------");
-                System.out.println(buscarPorCodigo(listaProdutos));
-                break;
-            case 3:
-                System.out.println("---------- Menu Duplicar Produtos ----------");
-                System.out.println("Digite o indíce do produto que deseja duplicar: ");
-                int indice = sc.nextInt();
-                escolherProdutoEDuplicar(indice, listaProdutos);
-                System.out.println("Produto duplicado!");
-                System.out.println("Digite enter para ver a nova lista");
-                sc.nextLine(); sc.nextLine();
-                imprimirProdutos(listaProdutos);
-                break;
-            default:
-                System.out.println("Opção inválida!");
-                break;
+        while (opcao != 4) {
+            System.out.println("-------------------------------------------");
+            System.out.println("Boas vindas ao seu gerenciador de estoques!");
+            System.out.println("-------------------------------------------");
+
+            System.out.println("---------- Menu Principal ----------");
+            System.out.println("1. Ver estoque \n2. Buscar produto \n3. Duplicar Produtos \n4. Sair");
+            System.out.print("Digite a opção que você deseja: ");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("--------------------------- Menu Estoque ---------------------------");
+                    imprimirProdutos(listaProdutos);
+                    System.out.println("Digite * ENTER * para voltar ao Menu Principal");
+                    sc.nextLine(); sc.nextLine();
+                    break;
+                case 2:
+                    System.out.println("---------- Menu Buscar por Código de Barras ----------");
+                    System.out.println(buscarPorCodigo(listaProdutos));
+                    System.out.println("Digite * ENTER * para voltar ao Menu Principal");
+                    sc.nextLine(); sc.nextLine();
+                    break;
+                case 3:
+                    System.out.println("---------- Menu Duplicar Produtos ----------");
+                    System.out.println("Digite o índice do produto que deseja duplicar: ");
+                    int indice = sc.nextInt();
+                    escolherProdutoEDuplicar(indice, listaProdutos);
+                    System.out.println("Produto duplicado!");
+                    System.out.println("Digite enter para ver a nova lista");
+                    sc.nextLine(); sc.nextLine();
+                    imprimirProdutos(listaProdutos);
+                    System.out.println("Digite * ENTER * para voltar ao Menu Principal");
+                    sc.nextLine();
+                    break;
+                case 4:
+                    System.out.println("Saindo do programa...");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    System.out.println("Digite * ENTER * para voltar ao Menu Principal");
+                    sc.nextLine(); sc.nextLine();
+                    break;
+            }
         }
     }
 
@@ -66,7 +82,7 @@ public class Loja {
         return "O código de barras informado não foi identificado";
     }
 
-    public static void imprimirProdutos(List<Produtos> lista){
+    public static void imprimirProdutos(List<Produtos> lista) {
         int i = 1;
         for (Produtos produto : lista) {
             System.out.print("Produto " + i + ": ");
@@ -75,23 +91,21 @@ public class Loja {
         }
     }
 
-    public static void escolherProdutoEDuplicar(int indice, List<Produtos> lista){
+    public static void escolherProdutoEDuplicar(int indice, List<Produtos> lista) {
         Produtos prod = lista.get(indice);
         Produtos produtoNovo1 = null;
         Produtos produtoNovo2 = null;
-        if (prod instanceof Livros){
+        if (prod instanceof Livros) {
             produtoNovo1 = new Livros(prod.getCodigoDeBarras(), prod.getNome(), prod.getPreco(), ((Livros) prod).getAutor());
             produtoNovo2 = new Livros("52241", prod.getNome(), prod.getPreco(), ((Livros) prod).getAutor());
-        }
-        else if (prod instanceof CDs){
+        } else if (prod instanceof CDs) {
             produtoNovo1 = new CDs(prod.getCodigoDeBarras(), prod.getNome(), prod.getPreco(), ((CDs) prod).getNumeroDeFaixas());
             produtoNovo2 = new CDs("52241", prod.getNome(), prod.getPreco(), ((CDs) prod).getNumeroDeFaixas());
-        }
-        else if (prod instanceof DVDs){
+        } else if (prod instanceof DVDs) {
             produtoNovo1 = new DVDs(prod.getCodigoDeBarras(), prod.getNome(), prod.getPreco(), ((DVDs) prod).getDuracao());
             produtoNovo2 = new DVDs("52241", prod.getNome(), prod.getPreco(), ((DVDs) prod).getDuracao());
         }
-        if (produtoNovo1 != null && produtoNovo1.equals(prod) && produtoNovo2 != null ){
+        if (produtoNovo1 != null && produtoNovo1.equals(prod) && produtoNovo2 != null) {
             lista.add(produtoNovo1);
             lista.add(produtoNovo2);
         }

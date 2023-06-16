9f0f821fc67e5295ecc93e559cf5b079c4daaa89
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-06-16 12:24:04 -0300

    :sparkles: feat: Adicionado Menu ao Ex01_IN

--- src/Ex01_IN/Exercicio01.java
+++ src/Ex01_IN/Exercicio01.java
@@ -4,15 +4,63 @@ import java.util.Scanner;
 
 public class Exercicio01 {
     public static void main(String[] args) {
+        imprimirMenu();
+    }
+
+    public static void imprimirMenu(){
+        System.out.println("---------------------------------------");
+        System.out.println("Boas vindas ao controlador de lâmpadas!");
+        System.out.println("---------------------------------------");
+
+        System.out.println("Você gostaria de controlar qual tipo de lâmpada? ");
+        System.out.println("Digite 'F' para Fluorescente");
+        System.out.println("Digite 'I' para Incandescente");
+        System.out.print("Digite aqui: ");
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o tipo de lampada: (Incandescente/Fluorescente)" );
-        String tipoLampada = sc.next();
+        String digito = sc.next();
 
-        FabricaLampada fabricaLampada = new FabricaLampada();
-        Lampada lampada = (Lampada) fabricaLampada.construir(tipoLampada);
+        while (!(digito.equalsIgnoreCase("f") || digito.equalsIgnoreCase("i"))){
+            System.out.println("Digite 'F' para Fluorescente");
+            System.out.println("Digite 'I' para Incandescente");
+            System.out.print("Digite aqui: ");
+            digito = sc.next();
+        }
 
-        lampada.ligar();
-        lampada.desligar();
+        if (digito.equalsIgnoreCase("f")){
+            FabricaLampada fabricaLampada = new FabricaLampada();
+            Lampada lampada = (Lampada) fabricaLampada.construir("fluorescente");
 
+            System.out.println("------------------------------------------------");
+            System.out.println("Perfeito, vamos alterar sua lâmpada Fluorescente");
+            System.out.println("1- ligar\n2- desligar\nDigite a opção: ");
+            int opcao = sc.nextInt();
+            if (opcao == 1){
+                System.out.println("***************");
+                lampada.ligar();
+                System.out.println("**************");
+            } else {
+                System.out.println("***************");
+                lampada.desligar();
+                System.out.println("***************");
+            }
+        }
+        else if (digito.equalsIgnoreCase("i")){
+            FabricaLampada fabricaLampada = new FabricaLampada();
+            Lampada lampada = (Lampada) fabricaLampada.construir("incandescente");
+
+            System.out.println("------------------------------------------------");
+            System.out.println("Perfeito, vamos alterar sua lâmpada Fluorescente");
+            System.out.println("1- ligar\n2- desligar\nDigite a opção: ");
+            int opcao = sc.nextInt();
+            if (opcao == 1){
+                System.out.println("***************");
+                lampada.ligar();
+                System.out.println("***************");
+            } else {
+                System.out.println("***************");
+                lampada.desligar();
+                System.out.println("***************");
+            }
+        }
     }
 }
